328fd9abb245632ec9681e3ce23b62d1af16081a
Stefan Adrian Danaita <me@dsa.io>
2020-06-21 20:17:43 +0000

    Initial commit

---
+++ build.rs
@@ -0,0 +1,13 @@
+// build.rs
+use grpc_build::build;
+
+fn main() {
+    build(
+        "./protos",
+        "src/protogen",
+        true,
+        true,
+        true,
+    )
+        .unwrap();
+}
---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use std::env;
+use tonic::transport::Server;
+use futures::channel::mpsc;
+use crate::protogen::protos::mock::mock_service_server::{MockService, MockServiceServer};
+use tonic::{Response, Status, Request};
+use crate::protogen::protos::mock::{MockResponse, MockRequest};
+use std::time::{SystemTime, UNIX_EPOCH};
+use futures::SinkExt;
+
+mod protogen;
+
+#[derive(Debug, Default)]
+pub struct Mock {}
+
+#[tonic::async_trait]
+impl MockService for Mock {
+    type MockStream = mpsc::Receiver<Result<MockResponse, Status>>;
+
+    async fn mock(&self, request: Request<MockRequest>) -> Result<Response<Self::MockStream>, Status> {
+        let req = request.into_inner();
+        let (mut tx, rx) = mpsc::channel(req.buffer_size as usize);
+
+        tokio::spawn(async move {
+            let started_at = SystemTime::now();
+            let mut count = 1;
+
+            loop {
+                if SystemTime::now().duration_since(started_at).unwrap().as_secs() > req.ttl {
+                    break;
+                };
+
+                while count < req.number_of_streams {
+                    let now = SystemTime::now();
+
+                    println!("Sending stream {}...", count);
+                    match tx.send(Ok(MockResponse {
+                        stream_id: count,
+                        created_at: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()
+                    })).await {
+                        Ok(_) => {
+                            println!("Sent stream {} in {} nanos", count, SystemTime::now().duration_since(now).unwrap().as_nanos());
+                        },
+                        Err(_) => {},
+                    };
+
+                    count += 1;
+                }
+            }
+        });
+
+        Ok(Response::new(rx))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let port = match env::var("PORT") {
+        Ok(val) => val,
+        Err(_) => String::from("50052"),
+    };
+
+    println!("{}", port);
+
+    let address = format!("127.0.0.1:{}", port).parse()?;
+    let mock = Mock::default();
+
+    let mut server = Server::builder();
+
+    println!("GO FOR LAUNCH");
+    server
+        .add_service(MockServiceServer::new(mock))
+        .serve(address)
+        .await?;
+
+    Ok(())
+}
